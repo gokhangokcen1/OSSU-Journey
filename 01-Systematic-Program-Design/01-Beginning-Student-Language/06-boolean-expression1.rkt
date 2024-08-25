4765bbae088bc705dfd31204e80e614dfe3d9c35
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-08-25 12:34:27 +0300

    Create 06-boolean-expression1.rkt

---
+++ 01-Systematic-Program-Design/01-Beginning-Student-Language/06-boolean-expression1.rkt
@@ -0,0 +1,66 @@
+(require 2htdp/image)
+
+;true
+;false
+
+(define WIDTH 100)
+(define HEIGHT 100)
+
+;(> WIDTH HEIGHT)
+;(>= WIDTH HEIGHT)
+;
+;(= 1 2)
+;(= 1 1)
+;(> 3 9)
+
+;(string=? "foo" "bar")
+
+(define I1 (rectangle 10 20 "solid" "red"))
+(define I2 (rectangle 20 10 "solid" "blue"))
+
+;(< (image-width I1)
+;   (image-width I2))
+;
+;(if (< (image-width I1)
+;   (image-width I2))
+;    "tall"
+;    "wide")
+;
+;(if (> (image-width I1)
+;   (image-width I2))
+;    "tall"
+;    "wide")
+
+(if (< (image-width I2)
+       (image-height I2))
+    (image-width I2)
+    (image-height I2))
+
+
+(if (< (image-width .)
+       (image-height I2))
+    (image-width I2)
+    (image-height I2))
+
+(if (< 20
+       (image-height I2))
+    (image-width I2)
+    (image-height I2))
+
+(if (< 20
+       (image-height .))
+    (image-width I2)
+    (image-height I2))
+
+(if (< 20 10)
+    (image-width I2)
+    (image-height I2))
+
+(if false
+    (image-width I2)
+    (image-height I2))
+
+(image-height I2)
+(image-height .)
+10
+ 
