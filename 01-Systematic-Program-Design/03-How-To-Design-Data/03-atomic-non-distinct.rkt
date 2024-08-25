9dcc5e1368499da5a738b3827d6ce24c4235ad04
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-08-25 19:10:14 +0300

    Create 03-atomic-non-distinct.rkt

---
+++ 01-Systematic-Program-Design/03-How-To-Design-Data/03-atomic-non-distinct.rkt
@@ -0,0 +1,24 @@
+;; city-name-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; Imagine that you are designing a program that, among other things,
+; has information about the names of cities in its problem domain.
+; 
+; Design a data definition to represent the name of a city.
+; 
+
+
+
+;; CityName is String
+;; interp. the name of a city
+(define CN1 "Boston")
+(define CN2 "Vancouver")
+#;
+(define (fn-for-city-name cn)
+  (... cn))
+
+;; Tempate rules used:
+;;  - atomic non-distinct: String
+
