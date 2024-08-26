cb10bdb4dc049a608d18775caddbf7ce7c29105c
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-08-26 15:59:09 +0300

    Create 10-enumeration-bump-up.rkt

---
+++ 01-Systematic-Program-Design/03-How-To-Design-Data/10-enumeration-bump-up.rkt
@@ -0,0 +1,44 @@
+ ; 
+; PROBLEM:
+;  
+; AS part of designing a system to keep track of student grades, you
+; are asked to design a data definition to represent the letter grade
+; in a course, which one of A, B or C.
+; 
+
+;; Data definitions:
+
+;; LetterGrade is one of:
+;;  - "A"
+;;  - "B"
+;;  - "C"
+;; interp. the letter grade in a course
+;; <examples are redundant for enumerations>
+#;
+(define (fn-for-letter-grade lg)
+  (cond [(string=? lg "A") (...)]
+        [(string=? lg "B") (...)]
+        [(string=? lg "C") (...)]))
+
+;; Template rules used:
+;;  - one of: 3 cases
+;;  - atomic distinct value: "A"
+;;  - atomic distinct value: "B"
+;;  - atomic distinct value: "C"
+
+;; Functions:
+
+;; LetterGrade -> LetterGrade
+;; produce next highest letter grade (no change for A)
+(check-expect (bump-up "A") "A")
+(check-expect (bump-up "B") "A")
+(check-expect (bump-up "C") "B")
+
+;(define (bump-up lg) "A")  ;stub
+
+;<using template from LetterGrade>
+(define (bump-up lg)
+  (cond [(string=? lg "A") "A"]
+        [(string=? lg "B") "A"]
+        [(string=? lg "C") "B"]))
+
