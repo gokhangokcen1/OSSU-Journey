e1b7c2cdaefe556078d0b87b5bfbb005756c3b0c
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-09-01 17:20:18 +0300

    Create P07-even-more-foo-evaluation.rkt

---
+++ 01-Systematic-Program-Design/01-Beginning-Student-Language/P07-even-more-foo-evaluation.rkt
@@ -0,0 +1,32 @@
+
+;; even-more-foo-evaluation-starter.rkt
+
+; PROBLEM:
+; 
+; Given the following function definition:
+; 
+; (define (farfle s)
+;   (string-append s s))
+; 
+; Write out the step-by-step evaluation of the expression: 
+; 
+; (farfle (substring "abcdef" 0 2))
+; 
+; Be sure to show every intermediate evaluation step.
+; 
+
+
+
+(define (farfle s)
+  (string-append s s))
+
+(farfle (substring "abcdef" 0 2))
+
+;(farfle "ab")
+
+#;
+(define (farfle "ab")
+  (string-append "ab" "ab"))
+
+;"abab"
+
