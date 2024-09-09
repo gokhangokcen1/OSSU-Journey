d436caba5431fe769394185ecaa8c9db49be4f3f
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-09-09 16:29:37 +0300

    Create 01-list-mechanism.rkt

---
+++ 01-Systematic-Program-Design/06-Self-Reference/01-list-mechanism.rkt
@@ -0,0 +1,26 @@
+(require 2htdp/image)
+
+empty
+
+(define L1 (cons "Flames" empty))                ; a list of 1 element
+(define L2 (cons 10 (cons 9 (cons 10 empty))))   ; a list of 3 elements
+(define L3 (cons (square 10 "solid" "blue")      ; a list of 3 elements
+           (cons (triangle 20 "solid" "green")        
+                  empty)))                             
+
+
+
+(first L1)
+(first L2)
+(first L3)
+
+(rest L1)
+(rest L2)
+(rest L3)
+
+(first (rest L2)) ;how do I get the second element of L2
+(first (rest (rest L2))) ;how do I get the third element of L2
+
+(empty? empty)
+(empty? L1)
+;(empty? 1)
