70d6e66e362b32893ced9bf59e7b46f64e305322
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-09-11 20:29:34 +0300

    Create P5-largest.rkt

---
+++ 01-Systematic-Program-Design/06-Self-Reference/P5-largest.rkt
@@ -0,0 +1,59 @@
+
+;; largest-starter.rkt
+
+;; =================
+;; Data definitions:
+
+; 
+; Remember the data definition for a list of numbers we designed in Lecture 5f:
+; (if this data definition does not look familiar, please review the lecture)
+; 
+
+
+;; ListOfNumber is one of:
+;;  - empty
+;;  - (cons Number ListOfNumber)
+;; interp. a list of numbers
+(define LON1 empty)
+(define LON2 (cons 60 (cons 42 empty)))
+#;
+(define (fn-for-lon lon)
+  (cond [(empty? lon) (...)]
+        [else
+         (... (first lon)
+              (fn-for-lon (rest lon)))]))
+
+;; Template rules used:
+;;  - one of: 2 cases
+;;  - atomic distinct: empty
+;;  - compound: (cons Number ListOfNumber)
+;;  - self-reference: (rest lon) is ListOfNumber
+
+;; =================
+;; Functions:
+
+; 
+; PROBLEM:
+; 
+; Design a function that consumes a list of numbers and produces the largest number 
+; in the list. You may assume that all numbers in the list are greater than 0. If
+; the list is empty, produce 0.
+; 
+
+
+;; ListOfNumber -> ListOfNumber
+;; interp. the largest number
+(check-expect (largest empty) 0)
+(check-expect (largest LON2) 60)
+(check-expect (largest (cons 40 (cons 120 (cons 12 empty)))) 120)
+
+;(define (largest lon) 0) ;stub
+;<use template from ListOfNumber>
+
+(define (largest lon)
+  (cond [(empty? lon) 0]
+        [else
+         (if (> (first lon) (largest (rest lon)))
+             (first lon)
+             (largest (rest lon)))]))
+
