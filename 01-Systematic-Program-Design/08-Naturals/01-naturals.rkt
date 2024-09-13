969a667852f10bee752ea358e59477c5c7cd7b1d
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-09-13 18:23:14 +0300

    Create 01-naturals.rkt

---
+++ 01-Systematic-Program-Design/08-Naturals/01-naturals.rkt
@@ -0,0 +1,60 @@
+;; Natural is one of:
+;;  - 0
+;;  - (add1 Natural)
+;; interp. a natural number
+(define N0 0)         ;0
+(define N1 (add1 N0)) ;1
+(define N2 (add1 N1)) ;2
+
+(define (fn-for-natural n)
+  (cond [(zero? n) (...)]
+        [else
+         (... n
+              ;
+              ;
+              (fn-for-natural (sub1 n)))]))
+
+;; Template rules used:
+;;  - one-of: 2 cases
+;;  - atomic distinct: 0
+;;  - compound: (add1 Natural)
+;;  - self-reference: (sub1 n) is Natural
+
+; 
+; PROBLEMS:
+; 
+; (A) Design a function that consumes Natural number n and produces the sum of all
+;     the naturals in [0,n]
+; 
+; (B) Design a function that consumes Natural number n and produces a list of all
+;     the naturals of the form (cons n (cons n-1 ... empty)) not including 0.
+
+
+;; Natural -> Natural
+;; produce sum of Natural[0,n]
+(check-expect (sum 0) 0)
+(check-expect (sum 1) 1)
+(check-expect (sum 3) (+ 3 2 1 0))
+
+;(define (sum n) 0) ;stub
+
+(define (sum n)
+  (cond [(zero? n) 0]
+        [else
+         (+ n
+            (sum (sub1 n)))]))
+
+;; Natural -> ListOfNatural
+;; produce (cons n (cons n-1 ... empty)), not including 0
+(check-expect (to-list 0) empty)
+(check-expect (to-list 1) (cons 1 empty))
+(check-expect (to-list 2) (cons 2 (cons 1 empty)))
+
+;(define (to-list n) empty) ;stub
+
+(define (to-list n)
+  (cond [(zero? n) empty]
+        [else
+         (cons n
+              (to-list (sub1 n)))]))
+         
