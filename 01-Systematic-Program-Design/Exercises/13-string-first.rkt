6760ff8f57bd6eb00d8a83aeedd0f6c582cdab22
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2024-09-15 20:24:35 +0300

    Update 13-string-first.rkt

--- 01-Systematic-Program-Design/Exercises/13-string-first.rkt
+++ 01-Systematic-Program-Design/Exercises/13-string-first.rkt
@@ -1,9 +1,7 @@
 ; Exercise 13. Define the function string-first, which extracts the first
 ; 1String from a non-empty string. 
 
-
-(check-expect (string-first "hello world") "hello")
-(check-expect (string-first "gokhan gokcen") "gokhan")
-
 (define (string-first str)
   (substring str 0 1))
+
+(string-first "hello")
